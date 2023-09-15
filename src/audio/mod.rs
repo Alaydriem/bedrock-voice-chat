727cc49432ec64b42bfd16dc5bf1d1469a0ab608
Charles R. Portwood II <charlesportwoodii@erianna.com>
2023-09-14 20:36:09 -0500

    Audio is hard

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -54,113 +54,56 @@ impl From<AudioSampleRates> for u32 {
 }
 
 pub(crate) async fn stream_output(device: &cpal::Device) -> Result<(), anyhow::Error> {
-    let config = device.default_input_config().unwrap();
-    let sample_config: cpal::StreamConfig = StreamConfig {
-        channels: 2,
-        // Use the device sample rate, but it should be 48kHz. Enforce?
-        sample_rate: config.sample_rate(),
-        // 48kHz => 480 samples per channel, 960 per frame; 1920 bytes for Mono, 3840 for stereo
-        // 1920 bytes is per-channel - the data callback will be double (3840 at 48kHz)
-        // We want a fixed size so we can pack it into an Opus frame evenly
-        // Opus wants the frame length to be sizeof(float) * channel * frame_size
-        // If our input is 3840, the opus:: crate wil set the frame size to 960 per channel, which'll give us a 10ms frame_size
-        buffer_size: cpal::BufferSize::Fixed(1920),
-    };
-
+    let config: cpal::StreamConfig = StreamConfig { channels: 2, sample_rate: SampleRate(48000), buffer_size: cpal::BufferSize::Fixed(960) };
+    
     println!("{}", device.name().unwrap());
     let nsstream = std::net::TcpListener::bind("0.0.0.0:8444").unwrap();
-    let ring = ringbuf::HeapRb::<f32>::new(3840);
+    let c = config;
+    let latency_frames = (10.0 / 1_000.0) * c.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * c.channels as usize;
+    println!("{}", latency_samples);
+    let ring = ringbuf::HeapRb::<f32>::new(1920);
     let (mut producer, mut consumer) = ring.split();
-    for _ in 0..3840 {
+    for _ in 0..latency_samples {
         // The ring buffer has twice as much space as necessary to add latency here,
         // so this should never fail
         producer.push(0.0).unwrap();
     }
 
     let stream = match device.build_output_stream(
-        &sample_config,
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            // react to stream events and read or write stream data here.
-            for sample in data {
-                *sample = match consumer.pop() {
-                    Some(s) => s,
-                    None => {
-                        //println!("nothing");
-                        0.0
-                    }
-                };
-            }
-        },
-        move |err| {
-            println!("{}", err.to_string());
-        },
-        None, // None=blocking, Some(Duration)=timeout
-    ) {
-        Ok(stream) => stream,
-        Err(e) => return Err(anyhow!("{}", e.to_string())),
-    };
-
-    stream.play().unwrap();
-    for stream in nsstream.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut size_buffer = [0u8; 10];
-                match stream.peek(&mut size_buffer) {
-                    Ok(_) => {
-                        let sb = [
-                            size_buffer[0],
-                            size_buffer[1],
-                            size_buffer[2],
-                            size_buffer[3],
-                            size_buffer[4],
-                            size_buffer[5],
-                            size_buffer[6],
-                            size_buffer[7],
-                        ];
-                        let compressed_opus_len = usize::from_be_bytes(sb);
-                        let sample_length = AudioSampleRates::new(size_buffer[8]);
-                        let mut buffer = vec![0; compressed_opus_len];
-
-                        // Fill the buffer with the stream
-                        stream.read(&mut buffer).unwrap();
-
-                        let mut output: Vec<f32> =
-                            vec![0.0; (sample_length.get_frame_size() as usize) * 4];
-
-                        println!(
-                            "rec {} => {} | {:?}",
-                            compressed_opus_len,
-                            output.len(),
-                            sample_length
-                        );
-
-                        match opus::Decoder::new(sample_length.into(), opus::Channels::Stereo) {
-                            Ok(mut decoder) => match decoder.decode_float(
-                                &buffer[9..compressed_opus_len],
-                                &mut output,
-                                false,
-                            ) {
-                                Ok(size) => {
-                                    //println!("{:?} {:?}", size, output.len());
-                                    // size is the number of samples per channel
-                                    for sample in output {
-                                        match producer.push(sample.to_owned()) {
-                                            Ok(_) => {}
-                                            Err(_) => {}
-                                        }
-                                    }
-                                }
-                                Err(e) => {
-                                    println!("{:?}", e)
-                                }
-                            },
-                            Err(_) => {}
+            &c,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                // react to stream events and read or write stream data here.
+                for sample in data {
+                    *sample = match consumer.pop() {
+                        Some(s) => { s },
+                        None => {
+                            //println!("nothing");
+                            0.0
                         }
-                    }
-                    Err(_) => {}
-                };
-            }
-            Err(_) => {}
+                    };
+                }
+            },
+            move |err| {
+                // react to errors here.
+            },
+            None // None=blocking, Some(Duration)=timeout
+        ) {
+            Ok(stream) => stream,
+            Err(e) => return Err(anyhow!("{}", e.to_string()))
+        };
+    
+    stream.play().unwrap();
+    for s in nsstream.incoming() {
+        let mut rx_bytes = [0u8; 3840];
+        let mut ss = s.unwrap();
+        ss.read(&mut rx_bytes).unwrap();
+        let bytes = convert_u8_to_f32(&rx_bytes);
+        //println!("Recv: {:?}", bytes);
+
+        // todo!() downsample to mono
+        for sample in bytes {
+                producer.push(sample.to_owned()).unwrap_or({});
         }
     }
 
@@ -169,70 +112,62 @@ pub(crate) async fn stream_output(device: &cpal::Device) -> Result<(), anyhow::E
 }
 
 pub(crate) async fn stream_input(device: &cpal::Device) -> Result<(), anyhow::Error> {
-    let config = device.default_input_config().unwrap();
-    let sample_config: cpal::StreamConfig = StreamConfig {
-        channels: 2,
-        // Use the device sample rate, but it should be 48kHz. Enforce?
-        sample_rate: config.sample_rate(),
-        // 48kHz => 480 samples per channel, 960 per frame; 1920 bytes for Mono, 3840 for stereo
-        // 1920 bytes is per-channel - the data callback will be double (3840 at 48kHz)
-        // We want a fixed size so we can pack it into an Opus frame evenly
-        // Opus wants the frame length to be sizeof(float) * channel * frame_size
-        // If our input is 3840, the opus:: crate wil set the frame size to 960 per channel, which'll give us a 10ms frame_size
-        buffer_size: cpal::BufferSize::Fixed(1920),
-    };
+    // Input should be a mono channel
+    let config: cpal::StreamConfig = StreamConfig { channels: 2, sample_rate: SampleRate(48000), buffer_size: cpal::BufferSize::Fixed(960) };
 
     println!("{}", device.name().unwrap());
 
     let stream = match device.build_input_stream(
-        &sample_config,
-        move |data: &[f32], _: &cpal::InputCallbackInfo| {
-            //println!("\nsent {:?}", &data.len());
-            let sample_rate_e: AudioSampleRates = sample_config.sample_rate.0.into();
+        &config,
+        move |data: & [f32], _: &cpal::InputCallbackInfo| {
             let mut nsstream = TcpStream::connect("127.0.0.1:8444").unwrap();
             // We need a low & highpass filter, and a noise gate before transmitting
-            match opus::Encoder::new(
-                sample_rate_e.into(),
-                opus::Channels::Stereo,
-                opus::Application::Audio,
-            ) {
-                Ok(mut encoder) => match encoder.encode_vec_float(&data, data.len() * 2) {
-                    Ok(mut result) => {
-                        let size = result.len() + 9;
-                        let sr = (sample_rate_e as u32) as u8;
-                        let mut size_buffer = size.to_be_bytes().to_vec();
-                        size_buffer.push(sr);
-                        size_buffer.extend(result);
-
-                        println!("Sent {} => {}", data.len(), size);
-                        nsstream.write(size_buffer.as_ref()).unwrap();
-                        nsstream.flush().unwrap();
-                    }
-                    Err(e) => {
-                        println!("unable to encode {:?}", e);
-                    }
-                },
-                Err(e) => {
-                    println!("sending error {}", e.to_string());
-                }
-            };
+            let d = convert_f32_to_u8(&data);
+            nsstream.write(d).unwrap();
+            nsstream.flush().unwrap();
         },
         move |err| {
             // react to errors here.
             println!("{}", err.to_string());
         },
-        None, // None=blocking, Some(Duration)=timeout
+        None // None=blocking, Some(Duration)=timeout
     ) {
         Ok(stream) => stream,
-        Err(e) => return Err(anyhow!("{}", e.to_string())),
+        Err(e) => return Err(anyhow!("{}", e.to_string()))
     };
 
     stream.play().unwrap();
     loop {}
+    println!("exiting input");
+    Ok(())
 }
 
-pub(crate) async fn get_devices(
-    host: &cpal::platform::Host,
-) -> Result<(Option<cpal::Device>, Option<cpal::Device>), anyhow::Error> {
+pub(crate) async fn get_devices(host: &cpal::platform::Host) -> Result<(Option<cpal::Device>, Option<cpal::Device>), anyhow::Error> {
     Ok((host.default_input_device(), host.default_output_device()))
+}
+
+fn convert_f32_to_u8(input: &[f32]) -> &[u8] {
+    // Assuming the input slice is of the same length
+    let input_bytes = unsafe {
+        std::slice::from_raw_parts(
+            input.as_ptr() as *const u8,
+            input.len() * std::mem::size_of::<f32>(),
+        )
+    };
+
+    // Convert the bytes to &[u8]
+    unsafe { std::slice::from_raw_parts(input_bytes.as_ptr(), input_bytes.len()) }
+}
+
+fn convert_u8_to_f32(input: &[u8]) -> &[f32] {
+    // Assuming the input slice is of the same length
+    let input_f32 = unsafe {
+        std::slice::from_raw_parts(
+            input.as_ptr() as *const f32,
+            input.len() / std::mem::size_of::<f32>(),
+        )
+    };
+
+    // Convert the bytes to &[f32]
+    unsafe { std::slice::from_raw_parts(input_f32.as_ptr(), input_f32.len()) }
 }
