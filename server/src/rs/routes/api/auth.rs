f00f15bb9df0ef92d720ca09e76aeea20f853b58
Charles R. Portwood II <charlesportwoodii@erianna.com>
2023-12-20 16:45:00 -0600

    nothing

--- server/src/rs/routes/api/auth.rs
+++ server/src/rs/routes/api/auth.rs
@@ -1,4 +1,6 @@
+use anyhow::anyhow;
 use common::{
+    auth::xbl::ProfileResponse,
     ncryptflib as ncryptf,
     pool::redis::RedisDb,
     rocket::http::Status,
@@ -30,54 +32,79 @@ pub async fn authenticate(
     let client_id = config.minecraft.client_id.clone();
     let client_secret = config.minecraft.client_secret.clone();
 
-    match common::auth::xbl::server_authenticate_with_client_code(
+    let profile = match common::auth::xbl::server_authenticate_with_client_code(
         client_id,
         client_secret,
         oauth2_transaction_code,
     )
     .await
     {
-        Ok(params) => {
-            tracing::info!("{:?}", params);
-            let mut gamerpic: String = String::from("gamerpic");
-            let mut gamertag: String = String::from("gamertag");
+        // We should only ever get a single user back, if we get none, or more than one then...
+        // something not right
+        Ok(params) => match params.profile_users.len() {
+            0 => None,
+            1 => Some(params),
+            _ => None,
+        },
+        Err(e) => {
+            tracing::error!("{}", e.to_string());
+            None
+        }
+    };
 
-            // We should have at least one user, if we don't then we didn't get a valid response back
-            if params.profile_users.len() == 0 {
-                return JsonMessage::create(
-                    Status::Forbidden,
-                    None,
-                    None,
-                    Some("Unable to login to Microsoft Services"),
-                );
-            }
+    let (gamerpic, gamertag) = match get_gamertag_and_gamepic(profile) {
+        Ok((gamerpic, gamertag)) => (gamerpic, gamertag),
+        Err(e) => {
+            return JsonMessage::create(Status::Forbidden, None, None, Some(e.to_string().as_str()))
+        }
+    };
 
-            for setting in params.profile_users[0].settings.clone().into_iter() {
+    let response = LoginResponse {
+        key: String::from("foo"),
+        cert: String::from("foo2"),
+        gamerpic: gamerpic,
+        gamertag: gamertag,
+    };
+    return JsonMessage::create(Status::Ok, Some(response), None, None);
+
+    // Unauthorized by default as a fallthrough
+    return JsonMessage::create(
+        Status::Unauthorized,
+        None,
+        None,
+        Some("Unable to login to Microsoft Services"),
+    );
+}
+
+/// Extracts the gamerpicture and gamertag from the profile response
+fn get_gamertag_and_gamepic(
+    profile: Option<ProfileResponse>,
+) -> Result<(String, String), anyhow::Error> {
+    match profile {
+        Some(profile) => {
+            let mut gamerpic: Option<String> = None;
+            let mut gamertag: Option<String> = None;
+
+            for setting in profile.profile_users[0].settings.clone().into_iter() {
                 if setting.id.eq("GamerDisplayPicRaw") {
-                    gamerpic = setting.value.clone();
+                    gamerpic = Some(setting.value.clone());
                 }
 
                 if setting.id.eq("Gamertag") {
-                    gamertag = setting.value.clone();
+                    gamertag = Some(setting.value.clone());
                 }
             }
 
-            let response = LoginResponse {
-                key: String::from("foo"),
-                cert: String::from("foo2"),
-                gamerpic,
-                gamertag,
-            };
-            return JsonMessage::create(Status::Ok, Some(response), None, None);
+            if gamerpic.is_some() && gamertag.is_some() {
+                return Ok((gamerpic.unwrap(), gamertag.unwrap()));
+            }
+
+            return Err(anyhow!("Authentication was successful, but the profile did not include the expected attributes."));
         }
-        Err(e) => {
-            tracing::error!("{}", e.to_string());
-            return JsonMessage::create(
-                Status::Forbidden,
-                None,
-                None,
-                Some("Unable to login to Microsoft Services"),
-            );
+        None => {
+            return Err(anyhow!(
+                "Authentication to Microsoft services was unsucccessful."
+            ))
         }
     }
 }
