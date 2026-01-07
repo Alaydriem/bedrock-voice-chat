0ad4e2df6e734cb30059927ad0b56e3ceb76825e
Charles R. Portwood II <charlesportwoodii@erianna.com>
2026-01-06 21:13:14 -0600

    chore: release tagging

--- client/src-tauri/src/audio/stream/stream_manager/input.rs
+++ client/src-tauri/src/audio/stream/stream_manager/input.rs
@@ -360,7 +360,7 @@ impl InputStream {
                                     let thread_clone = thread.clone();
                                     std::thread::spawn(move || {
                                         while !shutdown_clone.load(Ordering::Relaxed) {
-                                            std::thread::sleep(Duration::from_millis(100));
+                                            std::thread::sleep(Duration::from_millis(500));
                                         }
                                         thread_clone.unpark();
                                     });
